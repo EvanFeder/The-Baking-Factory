291f8ece2014e4023466fa3c09337793f61d1d43
Evan Feder <evanmfeder@gmail.com>
2019-05-03 11:22:17 -0400

    Sessions controller authenticates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,18 @@ class SessionsController < ApplicationController
     end
     
     def create
-      user = User.authenticate(params[:username], params[:password]) # how to do??
-      if user
-        session[:user_id] = user.id
-        redirect_to home_path, notice: "Logged in!"
-      else
-        flash.now.alert = "Username and/or password is invalid"
-        render "new"
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to home_path, notice: "Logged in!"
+        else
+            flash.now.alert = "Username and/or password is invalid"
+            render "new"
       end
     end
     
     def destroy
-      session[:user_id] = nil
-      redirect_to home_path, notice: "Logged out!"
+        session[:user_id] = nil
+        redirect_to home_path, notice: "Logged out!"
     end
   end
